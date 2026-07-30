eb677f74609bc86eb922de76eb8c09a416efb04c
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-29 22:06:49 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ function getUserData(id) {
     db3: db3
   };
 }
+try{
+    const database = await central(id);
+
+    const results = await Promise.all([
+        dbs[database](id)
+        vault(id)
+    ]);
+}
+
+const dbInfo = results[0];
+const valutInfo = results[1];
+
+return{
+id: id,
+name: valutInfo.name,
+email: valutInfo.email,
+adress: valutInfo.adress,
+phone: valutInfo.phone,
+username: dbInfo.username,
+website: dbInfo.website,
+company: dbInfo.company
+
+} catch (error){
+    throw error;
+
+}
+getUserData(1)
